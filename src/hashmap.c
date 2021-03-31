fbe3a4c5670469f41a65f77a84d22f08e75a6091
chrisrhayden <chrisrhayden@gmail.com>
2021-03-30 19:13:23 -0700

    adding comments and small bug fixes

--- src/hashmap.c
+++ src/hashmap.c
@@ -3,37 +3,58 @@
 
 #include "hashmap_base.h"
 
-/* init/create the hashmap base
+/** init the hashmap base
+ *
+ * this will allocate memory for the base struct and the table
+ *
+ * @param hash_func
+ *  the hash function should return a 64bit int aka uint64_t
+ *
+ *  @param comp_func
+ *   a function that should return true if the keys match and false if they dont
+ *
+ *  @param drop_func
+ *   a function that will receive the key and value for each entry
+ *
+ *  @param size
+ *   the size of the hash table
  */
-HashMapBase *init_hashmap_base(HashFunc hash_func, DropFunc drop_func,
-                               CompFunc comp_func, uint64_t size) {
-    HashMapBase *map = malloc(sizeof(*map));
+HashMapBase *init_hashmap_base(HashFunc hash_func, CompFunc comp_func,
+                               DropFunc drop_func, size_t size) {
+    HashMapBase *map = malloc(sizeof(HashMapBase));
 
-    map->table_size = size;
-    map->table = calloc(sizeof(Entry *), size);
+    if (map == NULL) {
+        return NULL;
+    }
+
+    map->table = calloc(size, sizeof(Entry *));
 
     if (map->table == NULL) {
         return NULL;
     }
 
+    map->table_size = size;
     map->current_size = 0;
 
     map->hash_func = hash_func;
-    map->drop_func = drop_func;
     map->comp_func = comp_func;
+    map->drop_func = drop_func;
 
     return map;
 }
 
 /** drop the hashmap table, entrys and values
  *
- * the values will not be freed if there is no drop_func
+ * the values will not be freed if there is no drop_func attached to the hashmap
  *
  * NOTE: the value check could be enforced from the drop_func but it
  * probably is safer to do that here and let the drop_func assume it
  * will always have valid data
+ *
+ * @param map
+ *  the hashmap base
  */
-void drop_table(HashMapBase *map, DropFunc drop_func) {
+void drop_table(HashMapBase *map) {
     Entry *entry;
     Entry *temp;
 
@@ -41,7 +62,7 @@ void drop_table(HashMapBase *map, DropFunc drop_func) {
         entry = map->table[i];
 
         while (entry != NULL) {
-            if (drop_func) {
+            if (map->drop_func) {
                 map->drop_func((void *)entry->key, entry->value);
             }
 
@@ -56,16 +77,29 @@ void drop_table(HashMapBase *map, DropFunc drop_func) {
     free(map->table);
 }
 
-/** drop the whole hashmap */
+/** drop the whole hashmap
+ *
+ * this will try and free the table and ultimately the HashMapBase struct
+ *
+ * @param map
+ *  the hashmap base
+ */
 void drop_hashmap_base(HashMapBase *map) {
     if (map->table) {
-        drop_table(map, map->drop_func);
+        drop_table(map);
     }
 
     free(map);
 }
 
-/** create an entry struct */
+/** create an entry struct
+ *
+ * @param key
+ *  a pointer to the key
+ *
+ * @param value
+ *  a pointer the value
+ */
 Entry *create_entry(const void *key, void *value) {
     Entry *entry = malloc(sizeof(Entry));
 
@@ -82,52 +116,16 @@ Entry *create_entry(const void *key, void *value) {
     return entry;
 }
 
-/** the actual insert function
+/** the internal insert function
  *
- * this is where entrys will acutely be entered in to the table/linked
- list
+ * this is where entrys will acutely be entered in to the table/linked list
+ *
+ * @param map
+ *  the hashmap base
+ *
+ * @param entry
+ *  a pointer to an Entry struct, these will not be reallocate
  */
-// enum HashMapResult _insert_hashmap(HashMapBase *map, Entry **entry) {
-//    uint64_t key_hash = map->hash_func((*entry)->key) & (map->table_size - 1);
-
-//    // if the bucket entry is null then set the new entry as the start of the
-//    // linked list for the given bucket
-//    if (map->table[key_hash] == NULL) {
-//        map->table[key_hash] = *entry;
-
-//        return Success;
-//    }
-
-//    // if there is data in a given bucket see if there is a matching key
-//    // if not then add the new entry to the end of the buckets list
-//    bool found = false;
-
-//    Entry *table_entry = map->table[key_hash];
-
-//    // check all entrys in the list
-//    //
-//    // if we are at the first entry and next is null the we skip the loop and
-//    go
-//    // to the last key check
-//    // else we check the key in the loop
-//    while (table_entry->next != NULL && !found) {
-//        if (map->comp_func(table_entry->key, (*entry)->key)) {
-//            found = true;
-//        } else {
-//            table_entry = table_entry->next;
-//        }
-//    }
-
-//    // check the last (or first) entry in the list
-//    if (found || map->comp_func(table_entry->key, (*entry)->key)) {
-//        return FailedToInsertDuplicate;
-//    }
-
-//    // just add the entry to the end of the list
-//    table_entry->next = *entry;
-
-//    return Success;
-//}
 enum HashMapResult _insert_hashmap(HashMapBase *map, Entry *entry) {
     uint64_t key_hash = map->hash_func(entry->key) & (map->table_size - 1);
 
@@ -139,8 +137,11 @@ enum HashMapResult _insert_hashmap(HashMapBase *map, Entry *entry) {
         return Success;
     }
 
-    // if there is data in a given bucket see if there is a matching key
-    // if not then add the new entry to the end of the buckets list
+    /*
+     * if there is data in a given bucket see if there is a matching key if not
+     * then add the new entry to the end of the buckets list
+     */
+
     bool found = false;
 
     Entry *table_entry = map->table[key_hash];
@@ -151,6 +152,7 @@ enum HashMapResult _insert_hashmap(HashMapBase *map, Entry *entry) {
     // to the last key check
     // else we check the key in the loop
     while (table_entry->next != NULL && !found) {
+        // check if key is already in the table
         if (map->comp_func(table_entry->key, entry->key)) {
             found = true;
         } else {
@@ -158,14 +160,15 @@ enum HashMapResult _insert_hashmap(HashMapBase *map, Entry *entry) {
         }
     }
 
-    // check the last (or first) entry in the list
+    // now check the last (or first) entry in the list for duplicates
     if (found || map->comp_func(table_entry->key, entry->key)) {
         return FailedToInsertDuplicate;
     }
 
-    // just add the entry to the end of the list
+    // just add the entry to the end of the list if there isn't a duplicate
     table_entry->next = entry;
 
+    // let the user know it was successful
     return Success;
 }
 
@@ -173,20 +176,23 @@ enum HashMapResult _insert_hashmap(HashMapBase *map, Entry *entry) {
  *
  * this will rehash the whole table to a bigger size based on the GROWTH_FACTOR
  *
- * it would be faster to just give the new table the old entrys but this is
- * fine for now
+ * this will not reallocate entrys, this i kind of dangerous as it will leave
+ * chains/linked lists half made if there is ever a problem
+ *
+ * @param map
+ *  the hashmap base
  */
 enum HashMapResult rehash_hashmap(HashMapBase *map) {
     enum HashMapResult result = Success;
 
+    // get the new table size
     int new_table_size = map->table_size * GROWTH_FACTOR;
 
-    HashMapBase *temp_map = init_hashmap_base(map->hash_func, map->drop_func,
-                                              map->comp_func, new_table_size);
-
+    // get a new base map with a larger table to insert in to
+    HashMapBase *temp_map = init_hashmap_base(map->hash_func, map->comp_func,
+                                              map->drop_func, new_table_size);
     if (temp_map == NULL) {
-        // this is technically true i guess
-        return FailedToInsertNoMemory;
+        return FailedToRehashNoMemory;
     }
 
     Entry *entry = NULL;
@@ -198,9 +204,13 @@ enum HashMapResult rehash_hashmap(HashMapBase *map) {
         entry = map->table[i];
 
         while (entry != NULL && result == Success) {
-
             temp_entry = entry->next;
 
+            // this is necessary else the insert function might/will find the
+            // same entry its trying to rehash
+            //
+            // though this is also dangerous as it will leave the old table
+            // incomplete if there is an issue inserting or rehashing
             entry->next = NULL;
 
             result = _insert_hashmap(temp_map, entry);
@@ -211,26 +221,26 @@ enum HashMapResult rehash_hashmap(HashMapBase *map) {
 
     // if there was an error drop the temp and let the user know
     if (result != Success) {
-        drop_hashmap_base(temp_map);
+        // dont free the entrys as data will still be in the original table and
+        // the user might want to do something with them
+        //
+        // TODO: this wont work right now, but I might switch to open addressing
+        // so the table wont be left in an incomplete state if there are errors
+        // during the rehashing
+        free(temp_map->table);
+        free(temp_map);
 
     } else { // assign the new table to the user's hashmap
+
         // drop the old table but dont drop the values
-        // drop_table(map, NULL);
         free(map->table);
 
         // set the new table to the old hashmap
         map->table = temp_map->table;
         map->table_size = temp_map->table_size;
 
-        // ignore the temp table when dropping as its now the new table for the
-        // users map
-        temp_map->table = NULL;
-
         // drop the temp map
         free(temp_map);
-
-        // this is probably redundant
-        result = Success;
     }
 
     return result;
@@ -238,8 +248,16 @@ enum HashMapResult rehash_hashmap(HashMapBase *map) {
 
 /** the insert entry point
  *
- * this will rehash the table when the current size is the same as the table
- * length * MAX_LOAD_FACTOR
+ * this will rehash when the table when it reaches the MAX_LOAD_FACTOR
+ *
+ * @param map
+ *  the hashmap base
+ *
+ * @param key
+ *  the new key to be inserted in to the hashmap
+ *
+ * @param value
+ *  the new value to be inserted in to the hasmap
  */
 enum HashMapResult insert_hashmap_base(HashMapBase *map, const void *key,
                                        void *value) {
@@ -259,6 +277,10 @@ enum HashMapResult insert_hashmap_base(HashMapBase *map, const void *key,
 
     Entry *entry = create_entry(key, value);
 
+    if (entry == NULL) {
+        return FailedToInsertNoMemory;
+    }
+
     result = _insert_hashmap(map, entry);
 
     if (result != Success) {
@@ -271,7 +293,14 @@ enum HashMapResult insert_hashmap_base(HashMapBase *map, const void *key,
     return Success;
 }
 
-// check is a key is in the table
+/** check is a key is in the table
+ *
+ * @param map
+ *  the hashmap base
+ *
+ * @param key
+ *  the key to check
+ */
 bool contains_key_hashmap_base(HashMapBase *map, const void *key) {
     uint64_t key_hash = map->hash_func(key) & (map->table_size - 1);
 
@@ -290,7 +319,16 @@ bool contains_key_hashmap_base(HashMapBase *map, const void *key) {
     return found;
 }
 
-/** delete the entry for the given key */
+/** delete the entry for the given key
+ *
+ * @param map
+ *  the hashmap base
+ *
+ * @param key
+ *  the key to find and remove
+ */
+// TODO: this could probably be improved but will also change a lot if i switch
+// to open addressing
 void *remove_entry_hashmap_base(HashMapBase *map, const void *key) {
     uint64_t key_hash = map->hash_func(key) & (map->table_size - 1);
 
@@ -316,6 +354,7 @@ void *remove_entry_hashmap_base(HashMapBase *map, const void *key) {
 
         entry = NULL;
 
+        --map->current_size;
         return value;
     }
 
@@ -340,12 +379,14 @@ void *remove_entry_hashmap_base(HashMapBase *map, const void *key) {
         }
     }
 
+    --map->current_size;
     return value;
 }
 
-/** get an iterator for a hashmap
+/** get an iterator struct for a hashmap
  *
- * this is definitely not thread safe
+ * @param map
+ *  the hashmap base
  */
 IterHashMap *get_iter_hashmap_base(HashMapBase *map) {
     IterHashMap *iter = malloc(sizeof(IterHashMap));
@@ -357,6 +398,8 @@ IterHashMap *get_iter_hashmap_base(HashMapBase *map) {
     iter->current_index = -1;
     iter->current_entry = NULL;
 
+    // set prev_index to something other then what current_index is being set to
+    // as to not match for the iter_next_safe_hashmap check at the begging
     iter->prev_index = -2;
     iter->prev_entry = NULL;
 
@@ -367,13 +410,31 @@ IterHashMap *get_iter_hashmap_base(HashMapBase *map) {
     return iter;
 }
 
+/** free the hashmap struct
+ *
+ * @param iter
+ *  the hashmap iter struct
+ */
 void drop_iter_hashmap(IterHashMap *iter) {
     free(iter);
 }
 
-/** next unsafe
+/** next (unsafe)
  *
  * it is not safe to mutate the table while iterating with this function
+ *
+ * this function will assign the current key and value to the given pointers
+ *
+ * @param iter
+ *  the hashmap iter struct
+ *
+ *  @param key
+ *   a pointer to another pointer that will hold each new key that gets iterated
+ *   over in the for each loop
+ *
+ *  @param value
+ *   a pointer to another pointer that will hold each new value that gets
+ *   iterated over during the for each loop
  */
 void iter_next_hashmap(IterHashMap *iter, const void **key, void **value) {
     // if current entry is not null then try and get the next Entry from the
@@ -410,11 +471,13 @@ void iter_next_hashmap(IterHashMap *iter, const void **key, void **value) {
     }
 }
 
-/** next safe
+/** next (remove safe)
  *
  * this will keep track of a previous entry and make sure that we get to the
- * right entry even if the user deletes an entry
+ * right entry even if the user deletes an entry, though its not "safe" in any
+ * other way
  */
+// NOTE: this is languishing as i dont care about it right now
 void iter_next_safe_hashmap(IterHashMap *iter, const void **key, void **value) {
     // if there is a previous entry and it has the same index as the current
     // entry then reset current to the last entry so we get to the right entry
@@ -479,8 +542,12 @@ void iter_next_safe_hashmap(IterHashMap *iter, const void **key, void **value) {
  */
 size_t data_hash64(const void *data, size_t len) {
     // cast the data to an 8bit pointer so we can iterate over all the data in
-    // memory using sizeof() to find the length, sizeof(THING) will return the
-    // amount of 8bit blocks for THING on most platforms as far as i know
+    // memory
+    //
+    // use sizeof() to find the length of an object
+    //
+    // sizeof(THING) will return the amount of 8bit blocks for THING on most
+    // platforms as far as i can tell
     const uint8_t *byte = (const uint8_t *)data;
     size_t hash = 0;
 
@@ -510,6 +577,11 @@ uint64_t integer_hash64(uint64_t x) {
     return x;
 }
 
+/** print a hasmap error to the console
+ *
+ * a simple function to print out what a HashMapResult is
+ * NOTE: it could be named better
+ */
 void print_hashmap_error(enum HashMapResult h_result) {
     switch (h_result) {
         case FailedToInsert: {
@@ -522,6 +594,10 @@ void print_hashmap_error(enum HashMapResult h_result) {
         }
         case FailedToInsertNoMemory: {
             printf("failed to insert -- no memory\n");
+            break;
+        }
+        case FailedToRehashNoMemory: {
+            printf("failed to rehash -- no memory\n");
             break;
         }
         case Success: {
