20a2f4818b9090512e929f0cdd4232a3675f799e
chrisrhayden <chrisrhayden@gmail.com>
2021-08-17 21:50:10 -0700

    test for_each_drop

--- test/main.c
+++ test/main.c
@@ -14,13 +14,14 @@
 // HASHMAP(HashMapData, struct TestStruct, struct TestStruct);
 HASHMAP(HashMapStr, char, char);
 
-uint64_t hash_data(const char *key) {
+uint64_t hash_data(char *key) {
     // int str_len = strnlen(key, INTMAX_MAX);
 
-    return data_hash64(key, sizeof(*key));
+    // return data_hash64(key, sizeof(*key));
+    return integer_hash64(*key);
 }
 
-bool comp_data_func(const char *key_1, const char *key_2) {
+bool comp_data_func(char *key_1, char *key_2) {
     return *key_1 == *key_2;
 }
 
@@ -37,9 +38,6 @@ HashMapStr *init_map() {
 }
 
 int main() {
-    // int iter_count = 1000;
-    int iter_count = 100;
-    int str_len = 5;
     HashMapStr *map = init_map();
 
     if (map == NULL) {
@@ -49,16 +47,15 @@ int main() {
 
     enum HashMapResult result = Success;
 
-    for (int i = 33; i < 44 && result == Success; ++i) {
-        char *test = malloc(sizeof(*test));
+    for (int i = 0x21; i <= 0x7E && result == Success; ++i) {
+        char *test_key = malloc(sizeof(*test_key));
 
-        *test = (char)i;
+        char *test_value = malloc(sizeof(*test_value));
 
-        // snprintf(test, sizeof(char) * str_len, "%d", i);
+        *test_key = (char)i;
+        *test_value = (char)i;
 
-        printf("%c\n", *test);
-
-        insert_hashmap(map, test, test, result);
+        insert_hashmap(map, test_key, test_value, result);
 
         if (result != Success) {
             printf("bad insert %d\n", i);
@@ -77,37 +74,7 @@ int main() {
         return 1;
     }
 
-    bool contains = false;
-    IterHashMap *iter;
-    get_iter_hashmap(map, iter);
-    char *iter_key;
-    char *value;
-
-    // this is silly but its just to have something to do with the hashmap
-    for_each(iter, iter_key, value) {
-        contains_key_hashmap(map, iter_key, contains);
-
-        if (contains == false) {
-            printf("could not find key\n");
-            break;
-        }
-    }
-
-    // char *key = malloc(sizeof(*key));
-    // contains = false;
-    // for (int i = 0; i < iter_count; ++i) {
-    //     snprintf(key, sizeof(char) * str_len, "%d", i);
-
-    //     contains_key_hashmap(map, key, contains);
-
-    //     if (contains == false) {
-    //         printf("did not find %s\n", key);
-    //         break;
-    //     }
-    // }
-
     void *return_value = NULL;
-
     char get_key = '+';
 
     get_value_hashmap(map, &get_key, return_value);
@@ -122,7 +89,39 @@ int main() {
 
     printf("longest chain %d\n", longest);
 
+    bool contains = false;
+    IterHashMap *iter;
+    get_iter_hashmap(map, iter);
+    char *iter_key;
+    char *value;
+
+    // this is silly but its just to have something to do with the hashmap
+    for_each(iter, iter_key, value) {
+        contains_key_hashmap(map, iter_key, contains);
+
+        if (contains == false) {
+            printf("could not find key\n");
+            break;
+        } else {
+            printf("> char is %c \n", *iter_key);
+        }
+    }
+
+    printf("%c\n", *iter_key);
+
+    iter_key = NULL;
+    value = NULL;
+
+    for_each_drop(iter, iter_key, value) {
+        printf("char %c\n", *iter_key);
+        free(iter_key);
+        free(value);
+    }
+
     drop_hashmap(map);
+
+    printf(">>>>>>>>>>> \n");
+
     printf("done\n");
 
     return 0;
